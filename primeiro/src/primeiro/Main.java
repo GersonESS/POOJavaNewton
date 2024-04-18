9c88174e1f3dcb29c4a01aa86b5896d29f596211
Gerson Santos <gersones@outlook.com>
2024-04-18 18:03:49 -0300

    a

---
+++ primeiro/src/primeiro/Main.java
@@ -0,0 +1,15 @@
+package primeiro;
+
+public class Main {
+	//import javax.swing.JOptionPane;
+
+	public static void main(String[] args) {
+		//String nome = JOptionPane.showInputDialog(null, "Qual é o seu nome?", "Digite seu nome", JOptionPane.QUESTION_MESSAGE);
+		JOptionPane.showMessageDialog(null, "Bem-Vindo nosso programa em JAVA da Turma OADS3 \n");
+
+	}
+
+}
+
+
+
