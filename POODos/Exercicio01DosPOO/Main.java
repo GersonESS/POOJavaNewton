bd1679ce4a72d44d6e71157cd68a19a2dc200325
Gerson Santos <gersones@outlook.com>
2024-04-20 17:49:50 -0300

    a

---
+++ POODos/Exercicio01DosPOO/Main.java
@@ -0,0 +1,11 @@
+import PacoteJavaDosPOO.Pessoa;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Bem Vindo Ao Game Room");
+        Pessoa objeto = new Pessoa();
+        objeto.apresentar();
+    }
+
+}
---
+++ POODos/Exercicio01DosPOO/PacoteJavaDosPOO/Pessoa.java
@@ -0,0 +1,8 @@
+package PacoteJavaDosPOO;
+
+public class Pessoa {
+    public void apresentar() {       
+        System.out.println("Gerson Analista De Sistema");
+        
+    }
+}
